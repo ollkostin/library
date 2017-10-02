d3691e9fb4b3787f6b8c298599d36df92f771638
Vladimir Kostin <ollkostin@gmail.com>
2017-10-02 23:58:55 +0300

    fix security (disable csrf)

--- src/main/java/ru/practice/kostin/library/config/SecurityConfig.java
+++ src/main/java/ru/practice/kostin/library/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package ru.practice.kostin.library.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,6 +12,17 @@ import ru.practice.kostin.library.service.CustomUserDetailsService;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private CustomUserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .permitAll();
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
