b654b89ce24f22272db5743a73762d2c3cb02ecf
Vladimir Kostin <ollkostin@gmail.com>
2017-10-15 00:39:29 +0300

    some fixes (error handler, dto's)

--- src/main/java/ru/practice/kostin/library/controller/ErrorHandlerController.java
+++ src/main/java/ru/practice/kostin/library/controller/ErrorHandlerController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import ru.practice.kostin.library.exception.BookAlreadyExistsException;
+import ru.practice.kostin.library.exception.UserAlreadyExistsException;
 import ru.practice.kostin.library.service.dto.ErrorDto;
 
 @ControllerAdvice
@@ -16,18 +17,19 @@ public class ErrorHandlerController {
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorDto notFound(NotFoundException e) {
-        return new ErrorDto(404, e.getMessage());
+        return new ErrorDto("404", e.getMessage());
     }
 
-    @ExceptionHandler(BookAlreadyExistsException.class)
-    @ResponseStatus(HttpStatus.FORBIDDEN)
-    public ErrorDto bookAlreadyExists(BookAlreadyExistsException e) {
-        return new ErrorDto(403, e.getMessage());
+    @ExceptionHandler({BookAlreadyExistsException.class, UserAlreadyExistsException.class})
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorDto bookAlreadyExists(Exception e) {
+        return new ErrorDto("409", e.getMessage());
     }
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
-    public ErrorDto illegalArgument(IllegalArgumentException e) {
-        return new ErrorDto(406, e.getMessage());
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorDto internalServerError(Exception e) {
+        return new ErrorDto("500", e.getMessage());
     }
+
 }
---
+++ src/main/java/ru/practice/kostin/library/exception/UserAlreadyExistsException.java
@@ -0,0 +1,12 @@
+package ru.practice.kostin.library.exception;
+
+public class UserAlreadyExistsException extends RuntimeException {
+
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+
+    public UserAlreadyExistsException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+}
