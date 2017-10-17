018e38aa96abc33a50487fa7aa4a65abb4cbdcac
Vladimir Kostin <ollkostin@gmail.com>
2017-10-17 23:56:17 +0300

    delete webjars, add static libs

--- src/main/java/ru/practice/kostin/library/config/MvcConfig.java
+++ src/main/java/ru/practice/kostin/library/config/MvcConfig.java
@@ -1,20 +1,11 @@
 package ru.practice.kostin.library.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        if (!registry.hasMappingForPattern("/webjars/**")) {
-            registry.addResourceHandler("/webjars/**")
-                    .addResourceLocations("classpath:/META-INF/resources/webjars/");
-        }
-    }
-
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addRedirectViewController("/","/books");
